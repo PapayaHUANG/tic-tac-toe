34f56946e84c898aed66996ebc7be708d2143e07
PapayaHUANG <hsy922@qq.com>
2022-08-05 19:03:42 +0800

    improved styling and added css animation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//变量声明
 let originBoard;
 let huPlayer;
 let aiPlayer;
+const circlePng = './circle.png';
+const crossPng = './cross.png';
 const restartButton = document.getElementById('restartButton');
 const xButton = document.getElementById('xButton');
 const oButton = document.getElementById('oButton');
@@ -15,42 +18,57 @@ const winCombos = [
   [2, 5, 8],
 ];
 
+//单元格绑定
 const cells = document.querySelectorAll('.cell');
+document.querySelector('.endgame').style.display = 'none';
 
-startGame();
+restartButton.addEventListener('click', reStartGame);
+xButton.addEventListener('click', chooseSign);
+oButton.addEventListener('click', chooseSign);
 
-restartButton.addEventListener('click', startGame);
-
-function startGame() {
+function reStartGame() {
   document.querySelector('.startgame').style.display = 'flex';
+  startGame();
+}
+
+//确认人机标识
+function chooseSign(e) {
+  if (e.target.id === 'xButton') {
+    huPlayer = crossPng;
+    aiPlayer = circlePng;
+  } else if (e.target.id === 'oButton') {
+    huPlayer = circlePng;
+    aiPlayer = crossPng;
+  }
+  //关闭提示框
+  document.querySelector('.startgame').style.display = 'none';
+  startGame();
+}
+function startGame() {
+  //控制提示框弹出和关闭
   document.querySelector('.endgame').style.display = 'none';
-  //bind div ID with board
+
+  //创建一个九宫格棋盘数组，元素为0-9
   originBoard = Array.from(Array(9).keys());
-  //remove Xs and Os
+
+  //给HTML上的单元格绑定事件监听器
   for (let cell of cells) {
-    cell.innerText = '';
     cell.addEventListener('click', turnClick);
     cell.style.removeProperty('background-color');
+    cell.innerHTML = '';
   }
+
+  //给提示框选标识绑定事件监听器
   xButton.addEventListener('click', chooseSign);
   oButton.addEventListener('click', chooseSign);
 }
 
-function chooseSign(e) {
-  if (e.target.id == 'xButton') {
-    huPlayer = 'X';
-    aiPlayer = 'O';
-  } else if (e.target.id == 'oButton') {
-    huPlayer = 'O';
-    aiPlayer = 'X';
-  }
-  document.querySelector('.startgame').style.display = 'none';
-}
+//确认人点击的九宫格
 function turnClick(square) {
   //square is equal to event in normal case;
   // Here I pass argu to func turn.
 
-  if (typeof originBoard[square.target.id] == 'number') {
+  if (typeof originBoard[square.target.id] === 'number') {
     turn(square.target.id, huPlayer);
     if (!checkTie()) {
       turn(bestSpot(), aiPlayer);
@@ -58,14 +76,16 @@ function turnClick(square) {
   }
 }
 
+//给点击的九宫格画上标记
 function turn(squareId, player) {
-  document.getElementById([squareId]).innerText = player;
-  document.getElementById([squareId]).style.color =
-    player == huPlayer ? 'white' : 'black';
+  const newChild = document.createElement('img');
+  newChild.src = player;
+  document.getElementById([squareId]).appendChild(newChild);
+
   //This syntax is for the sake of fuc checkWin.
   originBoard[squareId] = player;
   gameWon = checkWin(originBoard, player);
-  console.log(gameWon);
+
   if (gameWon) gameOver(gameWon);
 }
 
@@ -88,8 +108,9 @@ function checkWin(board, player) {
 
 function gameOver(gameWon) {
   for (let index of winCombos[gameWon.index]) {
-    document.getElementById(index).style.backgroundColor =
-      gameWon.player === huPlayer ? '#eb8d2f' : '#f0c5ed';
+    const selectedCells = document.getElementById(index);
+    selectedCells.style.backgroundColor = '#f2a84e';
+    selectedCells.style.animation = 'highlight 1s 4';
   }
   for (let cell of cells) {
     cell.removeEventListener('click', turnClick);
